2d8d1d0fb307d722ccb4819b8e6795c117b1c3ad
ShomaShirai <22t0837a@student.gs.chiba-u.jp>
2025-01-16 14:32:25 +0900

    プロジェクト ファイルを追加します。

---
+++ ss2409_pre/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ss2409_pre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1")});
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Add your event handling code here
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ss2409_pre/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Aruco;
+using OpenCvSharp.Extensions;
+
+namespace ss2409_pre
+{
+    public partial class Form1 : Form
+    {
+        private ComboBox comboBox2;
+        private VideoCapture capture;
+        private bool isRunning = false;
+        private Task captureTask;
+        private string _captureMode;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Click += new EventHandler(Button2_Click);
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            this.Text = "Arucoマーカ GUI";
+            this.Icon = new Icon("C:\\Users\\takos\\source\\repos\\ss2409_pre\\ss2409_pre\\favicon.ico");
+        }
+
+        private async void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                await StopCamera();
+                button2.Text = "開始";
+            }
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                await StopCamera();
+                button2.Text = "開始";
+            }
+            else
+            {
+                _captureMode = comboBox1.SelectedItem.ToString();
+
+                // モードがDummyの場合は早期リターン
+                if (_captureMode == "Dummy")
+                {
+                    MessageBox.Show("Dummyカメラは存在しません");
+                    return;
+                }
+
+                if (await StartCamera())
+                {
+                    button2.Text = "停止";
+                }
+                else
+                {
+                    MessageBox.Show("カメラの起動に失敗しました。");
+                }
+            }
+        }
+
+        private async Task<bool> StartCamera()
+        {
+            try
+            {
+                if (_captureMode == "USBカメラ")
+                {
+                    capture = new VideoCapture(0);
+
+                    if (capture == null || !capture.IsOpened())
+                    {
+                        MessageBox.Show("カメラが見つかりませんでした。");
+                        return false;
+                    }
+
+                    isRunning = true;
+                    captureTask = Task.Run(async () => await CaptureLoop());
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"キャプチャの起動中にエラーが発生しました: {ex.Message}");
+                return false;
+            }
+        }
+
+        private async Task StopCamera()
+        {
+            isRunning = false;
+            if (captureTask != null)
+            {
+                await captureTask;
+            }
+            capture?.Dispose();
+            capture = null;
+
+            if (pictureBox2.Image != null)
+            {
+                var temp = pictureBox2.Image;
+                pictureBox2.Image = null;
+                temp.Dispose();
+            }
+        }
+
+        private async Task CaptureLoop()
+        {
+            using (var frame = new Mat())
+            {
+                while (isRunning)
+                {
+                    try
+                    {
+                        Bitmap bitmap = null;
+                        if (_captureMode == "USBカメラ") // カメラモード
+                        {
+                            if (!capture.Read(frame) || frame.Empty())
+                                continue;
+                            bitmap = BitmapConverter.ToBitmap(frame);
+                        }
+
+                        if (bitmap != null)
+                        {
+                            if (pictureBox2.InvokeRequired)
+                            {
+                                pictureBox2.Invoke(new Action(() =>
+                                {
+                                    var oldImage = pictureBox2.Image;
+                                    pictureBox2.Image = bitmap;
+                                    oldImage?.Dispose();
+                                }));
+                            }
+                            else
+                            {
+                                var oldImage = pictureBox2.Image;
+                                pictureBox2.Image = bitmap;
+                                oldImage?.Dispose();
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"フレーム処理中にエラーが発生: {ex.Message}");
+                    }
+                    await Task.Delay(30); // フレームレートの制御
+                }
+            }
+        }
+
+        private async void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            await StopCamera();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
